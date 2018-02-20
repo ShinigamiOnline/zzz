c5e27fe76ed1f4ad042697ff02b93a5c4eb866e0
Shinigami <denshaotoko@outlook.com>
2018-02-20 17:46:05 +0100

    -----

---
+++ app/src/main/java/examen/org/caminodesantiagojorgeluisquinteros/GestionMunicipios.java
@@ -0,0 +1,13 @@
+package examen.org.caminodesantiagojorgeluisquinteros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GestionMunicipios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_municipios);
+    }
+}
